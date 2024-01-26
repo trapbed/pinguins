24b64eb96b2525a03c2e7e6c1138e822828fab29
trapbed <148848875+trapbed@users.noreply.github.com>
2024-01-26 15:08:51 +0500

    insert with check

--- insert.php
+++ insert.php
@@ -6,6 +6,35 @@
 
     include "connect.php"; //выражение include включает и выполняет указанный файл
 
-    $query_insert = "insert into news (`image`, `title`, `content`, `category_id`) VALUES ('$postImg[name]', '$postP', '$postTxt', $postCat)";
-    $insert = mysqli_query($con, $query_insert);//получаем результат хапроса из переменной Query_get_category 
+    $types=['image/png', 'image/jpg', 'image/jpeg'];
+    
+    foreach($types as $i){
+
+            if(mb_strlen($postP)<=20 && $postP!=="" &&  $postTxt!=="" && $postImg['type']==$i){
+                $query_insert = "insert into news (`image`, `title`, `content`, `category_id`) VALUES ('$postImg[name]', '$postP', '$postTxt', $postCat)";
+                $insert = mysqli_query($con, $query_insert);
+            }
+            else{
+                if(mb_strlen($postP)>20 ){
+                    echo "Слишком длинный заголовок <br>";
+                }
+                if($postP==""){
+                    echo "Пустой заголовок";
+                }
+                if(mb_strlen($postTxt)>20 ){
+                    echo "Слишком длинный текст заголовка <br>";
+                }
+                if($postTxt==""){
+                    echo "Пустой текст новости <br>";
+                } 
+                if ($postImg['type']!==$i){
+                    echo "Это не изображение";
+                }
+            }
+            break;
+
+
+        }
+    
+
 ?>
